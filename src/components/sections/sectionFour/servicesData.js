35d7ab98da17bcc22692ee9dcc8981a05e1b3901
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-15 16:39:13 +0200

    Create servicesData.js

---
+++ src/components/sections/sectionFour/servicesData.js
@@ -0,0 +1,59 @@
+import courseImg1 from "../../../assests/web-design.png";
+import courseImg2 from "../../../assests/graphics-design.png";
+import courseImg3 from "../../../assests/ui-ux.png";
+
+export const servicesData = [
+    {
+        id: "01",
+        title: "Development Websites - Front",
+        imgUrl: courseImg3,
+        clients: 16,
+        rating: 19.7,
+        count : 10
+    },
+    {
+        id: "02",
+        title: "Development Websites - Back",
+        imgUrl: courseImg2,
+        clients: 5.3,
+        rating: 19.7,
+        count : 33
+
+    },
+
+    {
+        id: "03",
+        title: "Development Websites - Front",
+        imgUrl: courseImg1,
+        clients: 53,
+        rating: 19.7,
+        count : 82
+
+    },
+
+    {
+        id: "04",
+        title: "Advertising On Soical Media",
+        imgUrl: courseImg2,
+        clients: 5,
+        rating: 19.5,
+        count : 43
+
+    },
+    {
+        id: "05",
+        title: "Advertising On Soical Media",
+        imgUrl: courseImg2,
+        clients: 53,
+        rating: 18.3,
+        count : 100
+
+    },
+    {
+        id: "06",
+        title: "Advertising On Soical Media",
+        imgUrl: courseImg3,
+        clients: 5.3,
+        rating: 55,
+    },
+]; 
