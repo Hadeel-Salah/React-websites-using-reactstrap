1f9e070c72805bd45ac94f0240af1957bc7af06e
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-15 16:38:12 +0200

    Create Services.jsx

---
+++ src/pages/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ServicesFirstSection from '../components/sections/servicesPage/sectionOne/ServicesFirstSection'
+import FeedbacksClients  from '../components/sections/servicesPage/sectionTwo/FeedbacksClients'
+
+export const Services = () => {
+  return (
+    <>
+      <ServicesFirstSection />
+      <FeedbacksClients />
+    </>
+  )
+}
