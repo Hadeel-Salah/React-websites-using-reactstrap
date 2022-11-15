dea65310615f7369b4194745a4f34cddbc156d32
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-15 16:39:51 +0200

    Create careersData.js

---
+++ src/components/sections/CareersSections/careersData.js
@@ -0,0 +1,26 @@
+export const careersData = [
+    {
+        id: "01",
+        title: "Software Engineer - FrontEnd",
+        image: 'ri-computer-fill',
+        url: ''
+    },
+
+    {
+        id: "02",
+        title: "Software Engineer - BackEnd",
+        image: 'ri-computer-fill',
+        url: ''
+
+
+    },
+
+    {
+        id: "03",
+        title: "Software Engineer - Product Team",
+        image: 'ri-computer-fill',
+        url: ''
+
+
+    },
+];
