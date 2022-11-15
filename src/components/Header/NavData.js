5ad580b9ad4cb4bb4e3f52e5893e70562237a254
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-15 16:40:24 +0200

    Create NavData.js

---
+++ src/components/Header/NavData.js
@@ -0,0 +1,23 @@
+export const navData = [
+    {
+        display: "Home",
+        url: "/",
+    },
+    {
+        display: "About",
+        url: "/about",
+    },
+
+    {
+        display: "Services",
+        url: "/Services",
+    },
+    {
+        display: "Careers",
+        url: "/careers",
+    },
+    {
+        display: "Results",
+        url: "/results",
+    },
+];
