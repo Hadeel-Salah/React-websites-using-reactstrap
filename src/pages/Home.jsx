d9c845531165dc19837e78c45b3d645dd3ce6da4
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-15 16:38:21 +0200

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FirstSection } from '../components/sections/sectionOne/FirstSection'
+import { SecondSection } from '../components/sections/sectionTwo/SecondSection'
+import FourthSection  from '../components/sections/sectionFour/FourthSection';
+
+function Home() {
+  return (
+    <>
+      <FirstSection />
+      <SecondSection />
+      <FourthSection />
+
+    </>
+  )
+}
+
+export default Home
