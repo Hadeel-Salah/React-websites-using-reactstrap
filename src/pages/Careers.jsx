0359819759fdae4e3e28f946e2da68d4f712a663
hadeelsala7 <h.salah.technical@gmail.com>
2022-11-15 16:38:14 +0200

    Create Careers.jsx

---
+++ src/pages/Careers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CurrentJobs from '../components/sections/CareersSections/currentJobs/CurrentJobs'
+import Jobs from '../components/sections/CareersSections/jobs/Jobs'
+export const Careers = () => {
+  return (
+    <>
+      <Jobs />
+      <CurrentJobs />
+    </>
+  )
+}
